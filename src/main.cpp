b2a44f9d565ba8e2bb9329600cdd6fc485744b4f
trnciii <doityan223@gmail.com>
2020-10-10 04:16:31 +0900

    rename file

--- src/main.cpp
+++ src/main.cpp
@@ -8,18 +8,28 @@
 display関数の中、glBegin(GL_LINES); と glEnd();　の中に点を並べると、
 その点の間が直線でつながれます。
 ----------------------------------------------------------------------------*/
-//#define GLFW_DLL
+
 #define GL_SILENCE_DEPRECATION
 #include "GLFW/glfw3.h"
 #include <math.h>
 
-//--初期化処理------------------------------------------------------------------
+//--初期化—------------------------------------------------------------------
 void myinit(GLFWwindow** window)
 {
     glfwInit();
     *window = glfwCreateWindow(600, 600, "lines", NULL, NULL); // 600*600の大きさで lines という名前のウィンドウをつくる
     glfwMakeContextCurrent(*window);
     glClearColor(0, 0, 0, 1); // 背景色の設定
+}
+
+
+//--再描画--------------------------------------------------------------------
+void reshape(GLFWwindow* window, int w, int h)
+{
+    glViewport( 0, 0, (GLsizei)w, (GLsizei)h ); //ウィンドウ全体をビューポートにする
+    glMatrixMode( GL_PROJECTION );
+    glLoadIdentity();
+    glOrtho(-1.0f, 1.0f, -1.0f, 1.0f, -1.0f, 1.0f); //視野の指定
 }
 
 //--描画内容--------------------------------------------------------------------
@@ -40,19 +50,23 @@ void display(void){
 int main(void)
 {
     //初期化処理 (ウィンドウサイズや表示位置、タイトル、背景色、...を決める)
-    //----------------------------------------------------------------
     GLFWwindow* window; // ウィンドウの変数をつくる
     myinit(&window); // ウィンドウのポインタを渡して初期化
     
+    
+    //コールバック関数 (キーボードやマウスの動きを捕まえる)
+    glfwSetWindowSizeCallback(window, reshape);
+    
+    
     //イベント処理ループ
-    //--------------------------------------------------------
     do{
         display();
         glfwSwapBuffers(window);
         glfwPollEvents();
     }while( !glfwWindowShouldClose(window) );//ウィンドウが表示されるかぎりループする
 
-    glfwTerminate();
+   glfwTerminate();
 
     return 0;
 }
+
